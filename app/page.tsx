59c12098cfa7650ed20811fcee05c3a12dbb0c88
Umar Faruq <umarfaruq@assetfindr.com>
2024-06-03 13:51:44 +0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <p className="text-green-400 font-semibold text-lg">
-            Refreshment App
+            Refreshment App For Learn
           </p>
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
